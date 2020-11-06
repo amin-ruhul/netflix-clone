9ac9624af3ed78c530585bed0a401f766b25d6af
Ruhul Amin <ruhulinfo50@gmail.com>
2020-11-06 12:04:16 +0600

    video trailer

--- src/Banner.js
+++ src/Banner.js
@@ -18,7 +18,6 @@ function Banner() {
         fetchDate(); 
     }, []);
 
-    console.log(movie);
 
     function truncate(str,n){
         return str?.length>n ? str.substr(0,n-1) + '...'  : str;
